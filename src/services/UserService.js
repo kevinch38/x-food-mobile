ba88d626b5a8b07e94bbc874bb2ab856d22eac75
Febri Gilang <febri.gilangpratama22@gmail.com>
2023-12-15 16:13:18 +0700

    [FIX]: fix error register

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
-export const register = async (user) => {
-  const { data } = await axios.post(
-    `http://localhost:8080/api/users/register`,
-    user
-  );
-  return data;
-};
 
 const UserService = () => {
-  const fetchUserById = async (id) => {
-    const { data } = await axios.get(`http://localhost:8080/api/users/${id}`);
-    return data;
-  };
+    const register = async (user) => {
+        const data = await axios.post(
+            `http://10.0.2.2:8083/api/users/register`,
+            user,
+        );
+        return data;
+    };
+    const fetchUserById = async (id) => {
+        const { data } = await axios.get(
+            `http://10.0.2.2:8083/api/users/${id}`,
+        );
+        return data;
+    };
 
-  return fetchUserById;
+    return { register, fetchUserById };
 };
 
 export default UserService;
