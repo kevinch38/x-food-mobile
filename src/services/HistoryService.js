7f924c1c85205c89f8896d6dfd66184d27ff0f74
raswan <rasonesualdi@gmail.com>
2024-01-03 14:34:01 +0700

    [FEATURE]->History order feature

---
+++ src/services/HistoryService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const HistoryService = () => {
+    const getAllOrderByAccountId = async (id) => {
+        try {
+            const response = await axios.get(`http://10.0.2.2:8087/api/orders?accountID=${id}`);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching order data:", error);
+            throw error;
+        }
+    }
+    return{
+        getAllOrderByAccountId,
+    }
+};
+
+export default HistoryService;
